3b3fcf7a7ad128b454ad02eefa4670a2dd737383
ympek <sz@ympek.net>
2020-05-21 18:32:10 +0200

    Zastepstwo 21.05.2020

--- DatabaseConsole.WPF/MainWindow.xaml.cs
+++ DatabaseConsole.WPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using DatabaseConsole.Data.Model;
 using DatabaseConsole.Data.Services;
 
 namespace DatabaseConsole.WPF
@@ -10,11 +12,13 @@ namespace DatabaseConsole.WPF
     public partial class MainWindow : Window
     {
         ContactsDAL contactsDAL;
+        Contact currentContact;
 
         public MainWindow()
         {
             InitializeComponent();
             contactsDAL = new ContactsDAL();
+            currentContact = new Contact();
             ReloadData();
         }
 
@@ -24,9 +28,33 @@ namespace DatabaseConsole.WPF
         }
         public void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            ClearFields();
-        }
 
+            // walidacja numeru telefonu
+            string phone = PhoneNumberTextbox.Text;
+
+            if (phone.Length != 9)
+            {
+                MessageBox.Show("Zły numer telefonu", "Błąd!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            currentContact.Name = NameTextbox.Text;
+            currentContact.Surname = SurnameTextbox.Text;
+            currentContact.Sex = (SexDictionary)SexCombobox.SelectedIndex + 1;
+            currentContact.PhoneNumber = PhoneNumberTextbox.Text;
+
+            if (currentContact.Id != 0)
+            {
+                contactsDAL.UpdateContact(currentContact);
+            }
+            else
+            {
+                contactsDAL.InsertContact(currentContact);
+            }
+            
+            ClearFields();
+            ReloadData();
+        }
 
         private void ReloadData()
         {
@@ -37,10 +65,45 @@ namespace DatabaseConsole.WPF
         {
             NameTextbox.Text = string.Empty;
             SurnameTextbox.Text = string.Empty;
-            SexTextbox.Text = string.Empty;
+            SexCombobox.SelectedIndex = 0;
             PhoneNumberTextbox.Text = string.Empty;
+            currentContact = new Contact();
         }
-        
 
+        private void DataContainer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            currentContact = (Contact)((ListView)sender).SelectedItem;
+
+            if (currentContact != null)
+            {
+                NameTextbox.Text = currentContact.Name;
+                SurnameTextbox.Text = currentContact.Surname;
+                SexCombobox.SelectedIndex = (int)currentContact.Sex - 1;
+                PhoneNumberTextbox.Text = currentContact.PhoneNumber;
+            }
+        }
+
+        public void NewButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearFields();
+        }
+
+        public void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentContact.Id != 0)
+            {
+                MessageBoxResult result = MessageBox.Show($"Czy jesteś pewien/pewna że chcesz usunąć: {currentContact.Name} {currentContact.Surname}",
+                                                          "Potwierdzenie",
+                                                          MessageBoxButton.YesNo,
+                                                          MessageBoxImage.Question);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    contactsDAL.DeleteContact(currentContact.Id);
+                }
+            }
+
+            ReloadData();
+        }
     }
 }
